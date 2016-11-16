d2a2f453213f69f0cf156a05455cd1f3f086a1c2
Andy Theuninck <andy@gohanman.com>
2016-11-16 14:19:14 -0600

    Trim whitespace out of submitted member names

--- fannie/classlib2.0/member/MemberREST.php
+++ fannie/classlib2.0/member/MemberREST.php
@@ -686,7 +686,7 @@ class MemberREST
                     if (isset($c_json['email'])) {
                         $meminfo->email_1($c_json['email']);
                     }
-                } elseif (isset($c_json['firstName']) && isset($c_json['lastName']) && $c_json['firstName'] == '' && $c_json['lastName'] == '') {
+                } elseif (isset($c_json['firstName']) && isset($c_json['lastName']) && trim($c_json['firstName']) == '' && trim($c_json['lastName']) == '') {
                     // blank name fields on non-account holder mean
                     // the customer was removed from the account
                     continue;
